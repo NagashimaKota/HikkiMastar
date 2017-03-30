c63895d5b16a6567da5eecf86fa6c9fa4a35aa7f
長島光太 <se.kokuto@gmail.com>
2017-03-31 03:27:00 +0900

    [add] キャラクターの移動
    デバッグがしやすいようにキャラクターの移動をつけた

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	private float leftPosition = -1.0f;
+    private float rightPosition = 1.5f;
+    private int screenWidth = Screen.width / 2;
+
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) )
+        {
+            this.transform.position = new Vector2( leftPosition, this.transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position = new Vector2(rightPosition, this.transform.position.y);
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch mytouch = Input.touches[0];
+
+            if(mytouch.position.x <= screenWidth)
+            {
+                this.transform.position = new Vector2(leftPosition, this.transform.position.y);
+            }
+            else
+            {
+                this.transform.position = new Vector2(rightPosition, this.transform.position.y);
+            }
+        }
 		
 	}
 }
